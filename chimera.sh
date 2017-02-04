64f8bc6307144225a83e3936377ff9001c01db51
syreal17 <syreal14@yahoo.com>
2017-02-04 00:27:33 -0800

    Changed some configs, added commenting support in chimera.sh

--- chimera.sh
+++ chimera.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 while IFS='' read -r build || [[ -n "$build" ]]; do
+	case "$build" in \#*) continue ;; esac
+
 	while IFS='' read -r invo || [[ -n "$invo" ]]; do
+		case "$invo" in \#*) continue ;; esac
+
 		printf "debug: $invo\n"
 		printf "debug: $build\n"
 		export build=$build
